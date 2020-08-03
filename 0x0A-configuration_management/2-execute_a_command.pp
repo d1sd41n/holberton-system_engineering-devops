c2091f339de263b519d5cc8dfeac2e12f6f29d75
Daniel Alejandro <outnumbered@live.com>
2020-08-03 18:48:43 -0500

    2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
-# creates a new conf file
-file { '/tmp/holberton':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  ensure  => 'file',
-  content => 'I love Puppet'
+# kills killmenow process
+
+exec { 'killmenow':
+  path    => '/usr/bin',
+  command => 'pkill  killmenow'
 }
