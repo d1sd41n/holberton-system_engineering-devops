e94961d026d91ea6478f22226a76b288478bc727
Daniel Pérez <34890805+d1sd41n@users.noreply.github.com>
2020-08-03 19:53:01 -0500

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # creates a new conf file
 
 file { '/tmp/holberton':
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-ensure => 'file',
-content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
