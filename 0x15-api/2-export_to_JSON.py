68791aebca3dac799282256697165c00c29cf7d8
dis dain <ddd666.dt@gmail.com>
2020-09-14 15:35:30 -0500

    2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""get data and saves it in a json"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get('{}/users/{}'.format(url, argv[1])).json()
+    todo = requests.get('{}/todos?userId={}'.format(url, argv[1])).json()
+    id_u = user.get('id')
+
+    with open('{}.json'.format(argv[1]), "w") as fil:
+        json_dict = {id_u: []}
+        for i in todo:
+            dict_ = {
+                'task': i.get('title'),
+                'completed': i.get('completed'),
+                'username': user.get('username')}
+            json_dict.get(user.get('id')).append(dict_)
+        json.dump(json_dict, fil)
