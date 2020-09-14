04c4ea2c49333f0eb6645983427c9b2cb53a7156
dis dain <ddd666.dt@gmail.com>
2020-09-14 15:15:58 -0500

    1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""get from from the api and saves it in a csv file"""
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    u_url = 'https://jsonplaceholder.typicode.com/' + 'users/' + argv[1]
+    response = requests.get(u_url)
+    user = response.json().get('username')
+    t_url = 'https://jsonplaceholder.typicode.com/' + 'todos'
+    response = requests.get(t_url)
+    todos = []
+    for x in response.json():
+        if x.get('userId') == int(argv[1]):
+            todos.append(x)
+    data_colums = ["userId", "name", "completed", "title"]
+    with open('{}.csv'.format(argv[1]), 'w') as fil:
+        writer = csv.DictWriter(fil, quoting=csv.QUOTE_ALL,
+                                fieldnames=data_colums)
+        for i in todos:
+            i['name'] = user
+            del i['id']
+            writer.writerow(i)
