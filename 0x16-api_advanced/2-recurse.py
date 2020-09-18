00090a268fafc5a6e5f753d738047e6bd1803c73
dis dain <ddd666.dt@gmail.com>
2020-09-17 20:11:56 -0500

    www

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,14 +10,13 @@ def recurse(subreddit, hot_list=[], after=''):
         subreddit, after)
     head = {'User-agent': 'qwerty'}
     res = requests.get(url, headers=head)
-    if res.status_code == 200:
-        data = res.json().get('data').get('children')
-        for i in data:
-            hot_list.append(i.get('data').get('title'))
-
-        aux = res.json().get('data').get('after')
-        if aux is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
-    else:
+    if res.status_code != 200:
         return 0
+    data = res.json().get('data').get('children')
+    for i in data:
+        hot_list.append(i.get('data').get('title'))
+
+    aux = res.json().get('data').get('after')
+    if aux is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
