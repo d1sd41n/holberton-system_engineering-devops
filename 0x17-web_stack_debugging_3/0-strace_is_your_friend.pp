9020c71a6677bb94d9d701af70c8c9b388a726a9
Daniel Perez <1498@holbertonschool.com>
2020-09-30 10:34:20 -0500

    fege

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,2 @@
 # fixing apache bug
-exec { 'fix_bug':
-  command => '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php':,
- }
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php':}
